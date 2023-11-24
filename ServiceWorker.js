f26c0ab83d5cca9736f3cdcb36a439325ed4b806
Phun Peeticharoenthum <phunphun484@gmail.com>
2023-11-24 22:32:40 +0700

    UPDATE: version 0.2.0

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Urnique Studio-GMM-0.1.4";
+const cacheName = "Urnique Studio-GMM-0.2.0";
 const contentToCache = [
     "Build/Builds.loader.js",
     "Build/Builds.framework.js",
